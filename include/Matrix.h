8bd7f2c264b3b320ce11e87cfe67ce9d697078eb
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-15 11:35:40 +0200

    Matrix header file

---
+++ include/Matrix.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<stdexcept>
+#include<vector>
+
+class Matrix {
+    private:
+        std::vector<std::vector<double>> data;
+        int rows;
+        int cols;
+
+    public:
+        Matrix(int rows, int cols);
+
+        Matrix add(const Matrix& other) const;
+        Matrix mult(const Matrix& other) const;
+        Matrix transpose() const;
+
+        int getRows() const;
+        int getCols() const;
+
+        double& operator()(int row, int col);
+        const double& operator()(int row, int col) const;
+};
