0ab1fff0df7088df81fa6cd74df58d0edb82bd96
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-23 23:44:25 +0200

    MSE calcLoss implemented

---
+++ src/MSE.cpp
@@ -0,0 +1,20 @@
+#include "../include/MSE.h"
+#include <stdexcept>
+
+double MSE::calcLoss(const Matrix& pred, const Matrix& target) const {
+    if (pred.getRows() != target.getRows() || pred.getCols() != target.getCols()) {
+        throw std::invalid_argument("Predicted and target matrices must have the same dimensions.");
+    }
+
+    double sum = 0.0;
+    int n = pred.getRows() * pred.getCols();
+
+    for (int i = 0; i < pred.getRows(); i++) {
+        for (int j = 0; j < pred.getCols(); j++) {
+            double diff = pred(i, j) - target(i, j);
+            sum += diff * diff;
+        }
+    }
+
+    return sum / n;
+}
