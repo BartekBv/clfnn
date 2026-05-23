5cac60012ffe129d1f9d929c0847c23d1dd38c42
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-23 23:47:29 +0200

    MSE calcGrad implemented

--- src/MSE.cpp
+++ src/MSE.cpp
@@ -18,3 +18,22 @@ double MSE::calcLoss(const Matrix& pred, const Matrix& target) const {
 
     return sum / n;
 }
+
+
+Matrix MSE::calcGrad(const Matrix& pred, const Matrix& target) const {
+    if(pred.getRows() != target.getRows() || pred.getCols() != target.getCols()) {
+        throw std::invalid_argument("Predicted and target matrices must have the same dimensions.");
+    }
+
+    int r = pred.getRows();
+    int c = pred.getCols();
+    Matrix grad(r, c);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            grad(i, j) = 2.0 * (pred(i, j) - target(i, j)) / (r * c);
+        }
+    }
+
+    return grad;
+}
