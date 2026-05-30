95d892f5e8ffb8e81b0926646a9ba26edd782a62
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-31 00:16:37 +0200

    feat: DenseLayer.h file added

---
+++ include/DenseLayer.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ILayer.h"
+#include "IActivation.h"
+#include "Matrix.h"
+
+class DenseLayer : public ILayer {
+    private:
+        Matrix weights;
+        Matrix biases;
+        Matrix lastInput;
+        Matrix lastZ;
+        Matrix current_dW;
+        Matrix current_dB;
+
+        IActivation* activation;
+    
+    public:
+        DenseLayer(int inputSize, int outputSize, IActivation* activation);
+
+        Matrix forward(const Matrix& input) override;
+        Matrix backward(const Matrix& outputGrad) override;
+        void updateWeights(double learningRate) override;
+
+        Matrix getWeights() const override;
+        Matrix getBiases() const override;
+};
