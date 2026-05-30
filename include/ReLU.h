48a842e70e3f883ef086545df99357e1e1b43b74
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-30 23:56:05 +0200

    wip: ReLU.h alpha parameter

--- include/ReLU.h
+++ include/ReLU.h
@@ -2,7 +2,11 @@
 #include "IActivation.h"
 
 class ReLU : public IActivation {
+    private:
+        double alpha; //leaky ReLU parameter
+
     public:
+        explicit ReLU(double alpha = 0.01);
         Matrix compute(const Matrix& input) const override;
         Matrix derivative(const Matrix& input) const override;
 };
