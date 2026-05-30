299b587de791e75101fb751dd38afb9b56914f3d
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-30 23:37:40 +0200

    wip: MSE.h create variable for LastCalculatedLoss and declare getLastCalculatedLoss()

--- include/MSE.h
+++ include/MSE.h
@@ -2,7 +2,10 @@
 #include "ILoss.h"
 
 class MSE : public ILoss {
+    private:
+        mutable double LastCalculatedLoss = 0.0;
     public:
         double calcLoss(const Matrix& pred, const Matrix& target) const override;
         Matrix calcGrad(const Matrix& pred, const Matrix& target) const override;
+        double getLastCalculatedLoss() const override;
 };
