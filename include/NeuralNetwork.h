3909b4364974b728314941b0f4febd3578f68499
BartekBv <bartekbudzinski1b@gmail.com>
2026-06-04 14:55:57 +0200

    wip: NauralNetwork.h class NeuralNetwork private

---
+++ include/NeuralNetwork.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Ilayer.h"
+#include "ILoss.h"
+#include "Matrix.h"
+#include <vector>
+
+class NeuralNetwork {
+    private:
+        std::vector<ILayer*> layers;
+        ILoss* lossFunction;
+
+    public:
+        ~NeuralNetwork();
+        NeuralNetwork(ILoss* lossFunction);
+        void addLayer(ILayer* layer);
+        void train(const Matrix& input, const Matrix& target, int epochs, double lr);
+        const Matrix predict(const Matrix& input);
+}
