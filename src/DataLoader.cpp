19563024c72a9bea943c96308e9672a8030c55ce
BartekBv <bartekbudzinski1b@gmail.com>
2026-06-10 11:53:34 +0200

    wip: in Dataloader.cpp add constructor

--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -4,3 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+
+
+DataLoader::DataLoader(const std::string& path, int incols, int outcols)
+    : filepath(path), inputCols(incols), outputCols(outcols) {}
