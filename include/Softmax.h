633c47869b9109bf3a3b5016abe294f6b8a79fd2
BartekBv <bartekbudzinski1b@gmail.com>
2026-06-10 13:57:56 +0200

    feat: Softmax.h

--- include/Softmax.h
+++ include/Softmax.h
@@ -1,3 +1,10 @@
 #pragma once
 #include "IActivation.h"
 #include "Matrix.h"
+
+class Softmax : public IActivation {
+    public:
+        Softmax() = default;
+        Matrix compute(const Matrix& input) const override;
+        Matrix derivative(const Matrix& input) const override;
+};
