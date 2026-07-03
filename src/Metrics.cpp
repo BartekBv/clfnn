2dfb09dcb686eef78b7b18ce70afb1e3902aab38
BartekBv <bartekbudzinski1b@gmail.com>
2026-07-04 00:06:24 +0200

    Metrics.cpp argmax function

--- src/Metrics.cpp
+++ src/Metrics.cpp
@@ -14,3 +14,15 @@ double Metrics::accuracy(const Matrix& pred, const Matrix& target) {
     }
     return (double)correct / predictions * 100.0;
 }
+
+int Metrics::argmax(const Matrix& matrix, int row) {
+    int mId = 0;
+    double mVal = matrix(row, 0);
+    for(int j = 1; j < matrix.getCols(); j++) {
+        if(matrix(row, j) > mVal) {
+            mVal = matrix(row, j);
+            mId = j;
+        }
+    }
+    return mId;
+}
